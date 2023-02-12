2eb0c7a34384c55f8e57e019718cdfd884fd50eb
Rahul Ranjan <rahulranjan@Rahuls-MacBook-Pro.local>
2023-02-12 20:19:46 +0530

    some_changes

--- src/redux/reducer/CartItemsReducer.tsx
+++ src/redux/reducer/CartItemsReducer.tsx
@@ -1,17 +1,11 @@
 import {GET_DATA, IS_LOGGED} from '../types';
-import {showMessage, hideMessage} from 'react-native-flash-message';
 
 const initialState = {
-  // downloadedMovies: [],
-  // allMovies: [],
-  // bookmarkMovies: [],
-  // onlyTrue: [],
-  // moviesList: [],
   uid: null,
   cartArray: [],
 };
 
-export default function CartItemsReducer(state = initialState, action) {
+export default function CartItemsReducer(state = initialState, action: any) {
   switch (action.type) {
     case IS_LOGGED:
       console.log('isLoggedInStatus', action.payload?.name);
